3938d305269e59ce8bfcfdfc1e51583b9def5860
ZehelFenris <43239740+ZehelFenris@users.noreply.github.com>
2026-08-18 03:09:39 +0200

    update

--- scripts.js
+++ scripts.js
@@ -1,4 +1,17 @@
+/*
 function toggleDiv(selectDiv) {
   const div = document.getElementById(selectDiv);
   div.classList.toggle("hidden");
+}
+*/
+function toggleDiv(selectDiv) {
+  const div = document.getElementById(selectDiv);
+  const button = document.querySelector(`button[data-target="${selectDiv}"]`);
+
+  div.classList.toggle("hidden");
+
+  if (button) {
+    const isHidden = div.classList.contains("hidden");
+    button.textContent = (isHidden ? "+" : "-");
+  }
 }
